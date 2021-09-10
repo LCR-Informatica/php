e9b14937942a9fcd86d0cf0da8867bfc091725f1
LCR-Informatica <lcrmgomes@gmail.com>
2021-09-10 14:52:35 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <p>FORMULARIO</p>
+        <form action="script.php" method="post">
+            <p>Seu Nome: <input type="text" name="nome" min="3" max="25" required/> </p>
+            <p>Sua Idade: <input type="number" name="idade" /></p>
+            <p><input type="submit" value="Enviar"></p>
+        </form>
+    </div>
+</body>
+
+</html>
