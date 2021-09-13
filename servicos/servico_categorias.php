2fd38978d7aedbe706213e91c4bdd0d00d1e86bf
LCR-Informatica <lcrmgomes@gmail.com>
2021-09-13 11:00:11 -0300

    Otimização das msgs e local das msgs de sucesso e erro

--- servicos/servico_categorias.php
+++ servicos/servico_categorias.php
@@ -3,19 +3,19 @@
 function defineCategoriaCompetidor(string $nome, string $idade): ?string
 {
   $categorias = ['infantil', 'adolecente', 'adulto'];
+  removeMsgError();
+  removeMsgSuccess();
   if (validaNome($nome) and validaIdade($idade)) {
-    removeMsgError();
-    removeMsgSuccess();
-    $msg = "O nadador $nome";
-    $msg2 = ", compete na categoria ";
+    $msg = "O nadador $nome, ";
+    $msg2 = "compete na categoria ";
     if ($idade > 5 and $idade < 13) {
       $msg .= $msg2 . $categorias[0];
     } elseif ($idade > 12 and $idade < 18) {
       $msg .= $msg2 . $categorias[1];
-    } elseif ($idade > 18) {
+    } elseif ($idade > 17 and $idade < 99) {
       $msg .= $msg2 . $categorias[2];
     } else {
-      $msg .= " não compete.";
+      $msg .= "não compete em nenhuma categoria.";
     }
     setMsgSuccess($msg);
     return NULL;
